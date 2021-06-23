4229645ef10f394993465c97fd81481ec572a12f
James Chandler <stillsound@protonmail.com>
2021-06-23 01:42:59 -0700

    change ids for five-day-forcast dd elements

--- css/js/script.js
+++ css/js/script.js
@@ -1,4 +1,4 @@
-
+//async function named getWeatherData
 async function getWeatherData(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -21,6 +21,7 @@ async function getWeatherData(e) {
     $('#temp').html(dataWeather.main.temp);
 };
 
+$('.cityWeather').on('submit', getWeatherData);
 
 async function getWeeklyForcast(e) {
     e.preventDefault();
@@ -37,13 +38,53 @@ async function getWeeklyForcast(e) {
     const dataForcast = await responseForcast.json();
 
     console.log(dataForcast);
-    
-     $('#dayOne').html(dataForcast.list[0].weather[0].description);
-     $('#dayTwo').html(dataForcast.list[1].weather[0].description);
-     $('#dayThree').html(dataForcast.list[2].weather[0].description);
-     $('#dayFour').html(dataForcast.list[3].weather[0].description);
-     $('#dayFive').html(dataForcast.list[4].weather[0].description);
 
-}
-$('.cityWeather').on('submit', getWeatherData);
+     $('#dayOne').html(dataForcast.list[0].weather[0].description);
+     $('#tempOne').html(dataForcast.list[0].main.temp);
+     $('#dateOne').html(dataForcast.list[0].dt_txt);
+
+     $('#dayTwo').html(dataForcast.list[8].weather[0].description);
+     $('#tempTwo').html(dataForcast.list[8].main.temp);
+     $('#dateTwo').html(dataForcast.list[8].dt_txt);
+
+     $('#dayThree').html(dataForcast.list[16].weather[0].description);
+     $('#tempThree').html(dataForcast.list[16].main.temp);
+     $('#dateThree').html(dataForcast.list[16].dt_txt);
+     
+     $('#dayFour').html(dataForcast.list[24].weather[0].description);
+     $('#tempFour').html(dataForcast.list[24].main.temp);
+     $('#dateFour').html(dataForcast.list[24].dt_txt);
+
+     $('#dayFive').html(dataForcast.list[32].weather[0].description);
+     $('#tempFive').html(dataForcast.list[32].main.temp);
+     $('#dateFive').html(dataForcast.list[32].dt_txt);
+
+};
+
 $('.cityWeather').on('submit', getWeeklyForcast);
+
+// async function (e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     const apiKey = '46bad53cdffa152d015bbe28e8a9a867';
+    
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+    
+
+//     //required name it what you want but needed
+//     const response = await fetch(url);
+//     //required same as above ^
+//     const data = await response.json();
+
+//     console.log(data);
+
+//     $('#').html();
+//     $('#').html();
+//     $('#').html();
+// };
+
+
+
+
+
